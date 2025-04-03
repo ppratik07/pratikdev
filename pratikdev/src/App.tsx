096e90ad79050e28e39b3dc6f0f26a465e5f8a5f
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-04-03 14:28:38 +0530

    added routes

--- pratikdev/src/App.tsx
+++ pratikdev/src/App.tsx
@@ -1,9 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { ProfilePage } from "./pages/ProfilePage"
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+       <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
